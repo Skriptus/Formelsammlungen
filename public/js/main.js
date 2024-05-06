62b13c3b649a0dec83dd211154d5d43535543387
michi <spass.mit.code@gmail.com>
2024-05-06 19:33:08 +0200

    dfsdkl

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,27 @@
 
-const Formeln=['Allgemeines','Chemie','Elektrotechnik','Gase','Grichisches Alphabet','Mathe','Physik','Stochastik','Thermodynamik'];
+const Formeln = {'Allgemein':['Allgemein','Grichisches Alphabet'],'Chemie':['Chemie'],
+'Elektrotechnik':['Elektrotechnik'],'Mathe':['Mathe','Stochastik'],
+'Physik': ['Physik','Gase','Thermodynamik']}
+const Formelcard = document.getElementById("Formelcard");
 const Formelbutton = document.getElementById("Formelbutton");
 const Formeltemp = document.getElementById("Formel");
 
 const Main = document.getElementById("Main");
 
-function AddFormel(Formel){
-  var element =  document.getElementById(Formel+"Formel");
+function AddFormel(FolderandFormel){
+  var Formel = FolderandFormel.split(",");
+  var element =  document.getElementById(Formel[1]+"Formel");
   if (element == null){
-    const Formelb = document.getElementById(Formel);
+    const Formelb = document.getElementById(FolderandFormel);
     Formelb.style.display = "none";
     const cloneFormel = Formeltemp.content.cloneNode(true);
     const new_formel = cloneFormel.querySelector(".Formel");
-    new_formel.src = "Formeln/"+Formel+".html";
-    new_formel.id = Formel+"Formel";
+    new_formel.src = "Formeln/"+Formel[0]+"/"+Formel[1]+".html";
+    new_formel.id = Formel[1]+"Formel";
     
     const x = cloneFormel.querySelector(".X");
     x.id = Formel+"x";
-    Main.prepend(cloneFormel);
+    document.getElementById(Formel[0]).prepend(cloneFormel);
     x.addEventListener("click", function() {
       const i = x.id;
       const f = i.substring(0,i.length-1);
@@ -34,22 +38,29 @@ function AddFormel(Formel){
 };
 
 
-for(let i=0; i<Formeln.length;i++){
+for(var key in Formeln){
+  const clonecard = Formelcard.content.cloneNode(true);
+  const new_card = clonecard.querySelector(".Formelcard");
+  new_card.textContent = Formeln[key][0];
+  new_card.id = Formeln[key][0];
+  // Append the cloned content to the parent element
+  Main.appendChild(new_card);
+  for (let ii=0; ii<Formeln[key].length;ii++){
+    
   const clonebutton = Formelbutton.content.cloneNode(true);
   const new_button = clonebutton.querySelector(".Formelbutton");
-  new_button.textContent = Formeln[i];
-  new_button.id = Formeln[i];
-  console.log(new_button.id);
+  new_button.textContent = Formeln[key][0]+","+Formeln[key][ii];
+  new_button.id = Formeln[key][0]+","+Formeln[key][ii];
   // Append the cloned content to the parent element
-  Main.appendChild(new_button);
+  document.getElementById(key).appendChild(new_button);
   // Add event listener for click events
   new_button.addEventListener("click", function() {
-    AddFormel(Formeln[i]); // Call the pre-defined function
+    AddFormel(new_button.id); // Call the pre-defined function
   });
   // Add event listener for touch events
   new_button.addEventListener("touchstart", function() {
-    AddFormel(Formeln[i]); // Call the pre-defined function
+    AddFormel(new_button.id); // Call the pre-defined function
   });
-};
+};};
 
 
